309972934e6e25a3e58c76f884edc7fe44f023c9
ptaxom <ptaxom@gmail.com>
2019-03-02 03:43:07 +0300

    settable parts and size

--- src/parent/parent.cpp
+++ src/parent/parent.cpp
@@ -12,38 +12,33 @@
 #include <vector>
 #include <stdlib.h>
 
-std::vector<std::complex<double>> points = 
-{
-{-1.5, 1},  {0, 0},
-{0, 1},     {1.5, 0},
-{-1.5, 0},  {0, -1},
-{0, 0},    {1.5, 1}};
+int width = 1000;
+int height;
+double x_start = -1.5, y_start = 1, x_end = 1.5, y_end = -1;
+int parts = 2;
 
+std::vector<std::complex<double>> points;
 
-int width = 600;
-int height = 400;
-
-std::vector<int> offsets = {0, 0, width, 0, 0, height, width, height};
+std::vector<int> offsets;
 
 template <class T>
-char* str_cast(T val)	
+char *str_cast(T val)
 {
 	std::string str = std::to_string(val);
-	char* cArr = (char*)malloc(str.length()+1);
+	char *cArr = (char *)malloc(str.length() + 1);
 	cArr[str.length()] = '\0';
-	for(int i = 0; i < str.length(); i++)
+	for (int i = 0; i < str.length(); i++)
 		cArr[i] = str[i];
 	return cArr;
 }
 
-
 void run_process(int num)
 {
-	char **args = (char**)malloc(sizeof(char*) * 8);
+	char **args = (char **)malloc(sizeof(char *) * 8);
 	args[0] = str_cast(offsets[2 * num]);
 	args[1] = str_cast(offsets[2 * num + 1]);
-	args[2] = str_cast(width);
-	args[3] = str_cast(height);
+	args[2] = str_cast(width / parts);
+	args[3] = str_cast(height / parts);
 	args[4] = str_cast(points[2 * num].real());
 	args[5] = str_cast(points[2 * num].imag());
 	args[6] = str_cast(points[2 * num + 1].real());
@@ -51,24 +46,54 @@ void run_process(int num)
 	execvp("./child", args);
 }
 
+void init()
+{
+	double dx = (x_end - x_start) / (double)parts;
+	double dy = (y_end - y_start) / (double)parts;
+	height = -width * dy / dx;
+	int dh = height / parts + 35;
+	int dw = width / parts ;
+	for (int y = 0; y < parts; y++)
+		for (int x = 0; x < parts; x++)
+		{
+			points.push_back({x_start + dx * x, y_start + dy * y});
+			points.push_back({x_start + dx * x + dx, y_start + dy * y + dy});
+			offsets.push_back(x * dw);
+			offsets.push_back(y * dh);
+		}
+}
+
 int main(int argc, char **argv)
 {
-	double x0 = -1.5, y0 = 1, x1 = 1.5, y1 = -1;
-	std::vector<int> order = {3, 2, 1, 0};
-
-	int threads = 0;
-	for(int i = 0; i < 4; i++)
+	if (argc == 3)
 	{
+		try
+		{
+			width = std::stoi(argv[1]);
+			parts = std::stoi(argv[2]);
+			if (width < 100 || width > 1600)
+				throw std::runtime_error("Invalid width!");
+			if (parts < 1 || parts > 4)
+				throw std::runtime_error("Invalid parts");
+			init();
+			for (int i = 0; i < parts * parts; i++)
+			{
 
-			pid_t cur_pid = fork();
-			if (cur_pid == 0){
-				run_process(order[i]);
-
+				pid_t cur_pid = fork();
+				if (cur_pid == 0)
+				{
+					usleep(1000 * 100);
+					run_process(parts * parts - i - 1);
+				}
 			}
-				
+			int status;
+			waitpid(0, &status, WNOHANG);
+		}
+		catch(const std::exception &ex)
+		{
+			std::cout << "Exception: " << ex.what() << std::endl; 
+		}
 	}
-	int status;
-	waitpid(0, &status, WNOHANG);
 
 	return 0;
 }
